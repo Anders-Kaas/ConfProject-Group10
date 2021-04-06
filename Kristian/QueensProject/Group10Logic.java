5ed73974cccd7e91f45e4cf78fd16c8d554a0d0e
Kristian Nørgaard Jensen <kristian.smed.noergaard@gmail.com>
2021-04-06 08:06:33 +0200

    Update Group10Logic.java

--- Kristian/QueensProject/Group10Logic.java
+++ Kristian/QueensProject/Group10Logic.java
@@ -3,7 +3,7 @@
  * Actually, when inserting a queen, it only puts the queen where requested
  * and does not keep track of which other positions are made illegal by this move.
  * 
- * @author Kristian Nørgaard Jesen
+ * @author Kristian Nørgaard Jensen, Anders Friis Kaas, Andreas Ørregaard Klingenbrunn
  * @version 04.04.2021
  */
 
